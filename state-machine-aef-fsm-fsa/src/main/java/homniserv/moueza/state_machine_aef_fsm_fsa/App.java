b8970c161c0b5455660a582b7636023ca2023600
Peter MOUEZA <mouezapeter@gmail.com>
2018-06-06 07:57:26 +0200

    start

---
+++ state-machine-aef-fsm-fsa/src/main/java/homniserv/moueza/state_machine_aef_fsm_fsa/App.java
@@ -0,0 +1,13 @@
+package homniserv.moueza.state_machine_aef_fsm_fsa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
